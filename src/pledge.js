09e3dbb07313eecc316284954e48e7b47299f8ac
rachel-rocks-at-ruby <rachelb@gmail.com>
2016-11-16 11:38:55 -0800

    first commit

---
+++ src/pledge.js
@@ -0,0 +1,26 @@
+'use strict';
+/*----------------------------------------------------------------
+Promises Workshop: build the pledge.js deferral-style promise library
+----------------------------------------------------------------*/
+// YOUR CODE HERE:
+
+
+
+
+
+
+/*-------------------------------------------------------
+The spec was designed to work with Test'Em, so we don't
+actually use module.exports. But here it is for reference:
+
+module.exports = {
+  defer: defer,
+};
+
+So in a Node-based project we could write things like this:
+
+var pledge = require('pledge');
+…
+var myDeferral = pledge.defer();
+var myPromise1 = myDeferral.$promise;
+--------------------------------------------------------*/
